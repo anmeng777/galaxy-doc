d04f8af326b6b3a020ddd0748f5edd0317d9a33d
bubble <137126891@qq.com>
2023-09-11 22:53:59 +0800

    初始化网站样式及模板

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    base : '',
+    title: 'Galaxy Docs',
+    description: '为了梦想，努力奋斗',
+	dest : "dist",
+    head : [
+		[
+			'link', {
+				rel : 'icon',
+				href : '/images/favicon.ico'
+			}
+		],
+		[
+			'link', {
+				rel : 'stylesheet',
+				href : '/css/ruoyi.css'
+			}
+		],
+		[
+			'script', {
+				charset : 'utf-8',
+				src : '/js/ruoyi.js'
+			}
+		]
+	],
+    markdown : {
+		lineNumbers : true // 代码块是否显示行号
+	},
+	themeConfig : {
+		sidebarDepth : 3,
+		nav : [ // 导航栏配置
+			{
+				text : 'java',
+				link : '/java/'
+			}
+		],
+		sidebar : {
+			'/java/' : [{
+					title : 'java学习文档',
+					collapsable : false,
+					children : [
+						'',
+						'document/kslj'
+					]
+				}
+			],
+			
+		}
+	}
+}
---
+++ docs/README.md
@@ -0,0 +1,18 @@
+---
+title: 主题标题栏的设置
+header-title: true
+home: true
+heroImage: /images/logo.png
+actionText: 开始奋斗 →
+actionLink: /java/
+features:
+  - title: 少儿编程
+    details: 比 Python 更实用，比 C++ 更简单
+  - title: 赴日转码
+    details: 更易就业，提前熟悉工作内容
+  - title: 国内转码
+    details: 免费，易学，长期更新
+  - title: 高中专业了解
+    details: 快速了解未来专业学习及工作内容
+footer: MIT Licensed | Copyright © 2023-2023 All Rights Reserved Galaxy
+---
